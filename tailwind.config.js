d08b871e8aa85b8350285099abb6c5f9dba7545a
SunnatulloValiyev <sunnatillovaliyev769@gmail.com>
2024-11-14 08:05:17 +0500

    change

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./.{html, css}"],
+  theme: {
+    extend: {
+      colors: {
+        'dark-green': '#079211',
+        'custom-gray': '#808080'
+      }
+    },
+
+  },
+  plugins: [],
+}
+
